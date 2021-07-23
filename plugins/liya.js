a3f552be4462c81ae568490985b95d193afffaea
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-07-24 00:26:07 +0530

    Update liya.js

--- plugins/liya.js
+++ plugins/liya.js
@@ -12,7 +12,7 @@ const lol = "need some word\n💘enthelum ezhuthu nenba"
 
     if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'liya ?(.*)', fromMe: true,dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'amal ?(.*)', fromMe: true,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(lol);
 
@@ -25,7 +25,7 @@ const lol = "need some word\n💘enthelum ezhuthu nenba"
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'liya ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'amal ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(lol);
 
