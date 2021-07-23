b9812c03fef4062cdfe86ce0216cfd07362a5347
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-07-23 22:40:01 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "http://imgur.com/gallery/A2zttE5.jpeg";
+  r_text[0] = "http://imgur.com/gallery/A2zttE5";
     
     
     var i = Math.floor(1*Math.random())
