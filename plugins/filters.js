3a7d45717e774babeaadd88bd79172a8871a16ed
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-09 15:56:43 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -123,7 +123,7 @@ await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/trance.
         if (!!message.mention && message.mention[0] == Config.AFNN) {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/mention.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
-const array = ['Mention']
+const array = ['ho','she','ra','mu','hot','ta','Cr7','uyir','power','va','wow','se','no','nanba','sebin','messi','maman']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
