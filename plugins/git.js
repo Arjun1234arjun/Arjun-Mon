f44d1054016022c3ceabfe6e39a2ce6657647d6b
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-11 18:48:27 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Ameer Suhail*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
 *owner number wa.me/919895828468*
 *bot setting video : https://youtu.be/VR_ppqEl2q8*
 *githublink       https://github.com/cyberchekuthan/Amalser_v1*
