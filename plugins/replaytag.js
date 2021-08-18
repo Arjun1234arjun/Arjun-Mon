9e4aae3ad581cb52c67a32137152c02e9b58d591
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-18 13:17:17 +0530

    Update replaytag.js

--- plugins/replaytag.js
+++ plugins/replaytag.js
@@ -3,7 +3,7 @@ const FormData = require('form-data')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, text }) => {
-  if (text) conn.sendFile(m.chat, 'TAG_REPLY' + encodeURIComponent(text), 'some.mp3', '', m, false, { asSticker: true })
+  if (text) conn.sendFile(m.chat, 'TAG_REPLY' + encodeURIComponent(text), 'Mention.mp3', '', m, false, { asSticker: true })
   else throw 'Uhm...Teksnya?'
 }
 handler.help = ['tagg <teks>']
