c729180afb0586339645ea3e7b3d229bf577fe78
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-25 18:06:02 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -55,3 +55,55 @@ Asena.addCommand({pattern: 'news ?(.*)', fromMe: false, desc: Lang.NEWS_DESC}, a
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDC, MessageType.text);
 	}
 });
+
+Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
+    if (message.jid === '919562803423-1627735504@g.us') {
+ 
+             return;
+         }
+         let regex1 = new RegExp('Bro')
+         let regex2 = new RegExp('name')
+         let regex3 = new RegExp('bot')
+         let regex4 = new RegExp('Amalser')
+         let regex5 = new RegExp('Amal')
+         let regex6 = new RegExp('git')
+         let regex7 = new RegExp('Who are you')
+         let regex8 = new RegExp('send')
+         let regex9 = new RegExp('Di')
+         let regex10 = new RegExp('bye')
+         let regex11 = new RegExp('Amal Supporters')
+         if (regex1.test(message.message)) {              
+             await message.client.sendMessage(message.jid,'to get all commands type .help', MessageType.text, {quoted: message.data })
+         } 
+         else if (regex2.test(message.message)) {
+            await message.client.sendMessage(message.jid,'my name is Amalser' , MessageType.text, {quoted: message.data })
+         }
+          else if (regex3.test(message.message)) {
+             await message.client.sendMessage(message.jid,'yes tell me', MessageType.text, {quoted: message.data })
+         }
+         else if (regex4.test(message.message)) {
+            await message.client.sendMessage(message.jid,'ʏᴇꜱ ᴛʜᴀᴛꜱ ᴍᴇ', MessageType.text, {quoted: message.data })
+         }
+         else if (regex5.test(message.message)) {
+           await message.client.sendMessage(message.jid,'i know him he is my creator', MessageType.text, {quoted: message.data })
+         }
+          else if (regex6.test(message.message)) {
+            await message.client.sendMessage(message.jid,'\n`Amalser v2 ❣️` \n*github.com/cyberchekuthan/Amalser_v1*', MessageType.text, {quoted: message.data })
+         }
+          else if (regex7.test(message.message)) {
+            await message.client.sendMessage(message.jid,'```I am Amalser.```', MessageType.text, {quoted: message.data })
+         }
+          else if (regex8.test(message.message)) {
+            await message.client.sendMessage(message.jid,'what', MessageType.text, {quoted: message.data })
+         }
+          else if (regex9.test(message.message)) {
+    await message.client.sendMessage(message.jid,'enthada kutta', MessageType.text, {quoted: message.data })
+         }
+         else if (regex10.test(message.message)) {
+    await message.client.sendMessage(message.jid,'```oh, ninnod arenkilm ivde nikkan paranjo..poo mister```', MessageType.text, {quoted: message.data })
+         }
+         else if (regex11.test(message.message)) {
+    await message.client.sendMessage(message.jid,'```Farhan ❤️ Afnan ❤️```', MessageType.text, {quoted: message.data })
+         }      
+     
+ }));
