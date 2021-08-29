928dbc00fe1956d3a85bdbdde3f17dab2386c1bf
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-29 12:59:39 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -124,7 +124,7 @@ const array = ['ya','apo','bomb','ee','list','machu','menu','mute','nee','onn','
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, {contextInfo: { forwardingScore: 1000, isForwarded: true }, mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, {contextInfo: { forwardingScore: Amalser❤️, isForwarded: true }, mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
     }
