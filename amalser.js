938d88dd77f96e3b7abb3e4d20e342f89770df19
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-29 14:35:52 +0530

    Update amalser.js

--- amalser.js
+++ amalser.js
@@ -13,9 +13,9 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
  OA_AI: process.env.Z_PINKY_AI === undefined ? true : process.env.Z_BOTNAME,  
- OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Amal' : process.env.Z_BOT_NAME,
- OA_NAME: process.env.Z_DEP_NAME === undefined ? 'Amal' : process.env.Z_DEP_NAME,
- OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'Amal is my creator his full name is amal' : process.env.Z_DEP_REPLY,
+ OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Pinky' : process.env.Z_BOT_NAME,
+ OA_NAME: process.env.Z_DEP_NAME === undefined ? 'Pinky' : process.env.Z_DEP_NAME,
+ OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'Plk is my creator his full name is afnanplk' : process.env.Z_DEP_REPLY,
     
 
 };
